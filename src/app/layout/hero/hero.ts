c187d2ed40f68ee84c8906b7fd7ce251ab18b702
wanjamueller <me@wanjamueller.com>
2026-09-24 13:56:31 +0200

    header created and logo implemented

---
+++ src/app/layout/hero/hero.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-hero',
+  styleUrl: './hero.scss',
+  templateUrl: './hero.html',
+})
+export class Hero {}
