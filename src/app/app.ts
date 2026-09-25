55715b573a71e89088eb1e79f5f0c2b0b6c8bc8f
wanjamueller <me@wanjamueller.com>
2026-09-25 13:33:53 +0200

    basic about done

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Hero } from './layout/hero/hero';
 import { Header } from './layout/header/header';
+import { About } from './layout/about/about';
 
 @Component({
-  imports: [RouterOutlet, Hero, Header],
+  imports: [RouterOutlet, Hero, Header, About],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
